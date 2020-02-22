47efb6833db3c572ac727b5b67d1c9bd2e362d65
atheler <atheler@users.noreply.github.com>
2020-02-22 20:56:07 +0100

    First block oscillators

---
+++ rhesuton/oscillators.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from config import BUFFER_SIZE
+from rhesuton.blocks import Block
+from rhesuton.waves import sample_wave
+
+
+class Oscillator(Block):
+    def __init__(self, frequency=440.):
+        super().__init__(nInputs=1, nOutputs=1)
+        self.frequency = self.input
+        self.frequency.set_value(frequency)
+        self.output.set_value(np.zeros(BUFFER_SIZE))
+        self.currentPhase = 0.
+
+    def update(self):
+        freq = self.frequency.get_value()
+        samples, self.currentPhase = sample_wave(BUFFER_SIZE, freq, self.currentPhase)
+        self.output.set_value(samples)
+
+
+class FmOscillator(Oscillator):
+    # TODO(atheler): Make me!
+    pass
+
+
+class WavetableOscillator(Oscillator):
+    # TODO(atheler): Make me!
+    pass
+
+
+class OvertoneOscillator(Oscillator):
+    # TODO(atheler): Make me!
+    pass
