1d885096ea6a43e621936d4e8bca9b9c4d580204
atheler <atheler@users.noreply.github.com>
2020-02-22 20:50:39 +0100

    Waves module

--- rhesuton/waves.py
+++ rhesuton/waves.py
@@ -3,34 +3,39 @@
 """
 import numpy as np
 
-from rhesuton.math import wrap
-from rhesuton.constants import TAU
 from config import SAMPLING_RATE
+from rhesuton.constants import TAU
+from rhesuton.math import wrap
 
 
 DT = 1. / SAMPLING_RATE
 
 
-def square_waveform(phase):
+def sine(phase):
+    """Sine wave function."""
+    return np.sin(phase)
+
+
+def square(phase):
     """Sqaure wave function."""
     return 2. * (wrap(phase) >= 0).astype(float) - 1.
 
 
-def sawtooth_waveform(phase):
+def sawtooth(phase):
     """Sawtooth wave function."""
     return 2. * np.mod(phase, TAU) / TAU - 1.
 
 
-def triangle_waveform(phase):
+def triangle(phase):
     """Triangle wave function."""
     return 1. - np.abs((4 * wrap(phase) / TAU) % 4 - 2)
 
 
 WAVE_FUNCTIONS = {
-    'sine': np.sin,
-    'square': square_waveform,
-    'sawtooth': sawtooth_waveform,
-    'triangle': triangle_waveform,
+    'sine': sine,
+    'square': square,
+    'sawtooth': sawtooth,
+    'triangle': triangle,
 }
 
 
