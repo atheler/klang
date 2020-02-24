3549fbc2fae14d3c3aec9d3b13e7b79103ceef03
atheler <atheler@users.noreply.github.com>
2020-02-24 09:31:33 +0100

    Rearanging

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
-SAMPLING_RATE = 44100
-"""int: WAV sampling rate."""
+ATTACK_RELEASE = 0.002
+"""float: Attack / release time for primitive linear fade in/out envelope."""
 
 BIT_DEPTH = 16
 """int: WAV bit depth."""
 
+BUFFER_SIZE = 256
+"""int: Internal buffer size."""
+
 KAMMERTON = 440.
 """float: Concert pitch frequency."""
 
-ATTACK_RELEASE = 0.002
-"""float: Attack / release time for primitive linear fade in/out envelope."""
+SAMPLING_RATE = 44100
+"""int: WAV sampling rate."""
 
 TEMPERAMENT_NAME = 'Young'
 """str: Temperament name for synthesizer."""
-BUFFER_SIZE = 1024
