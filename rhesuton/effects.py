cf587d7d488317f12519c24ba5ed554e0500aca1
atheler <atheler@users.noreply.github.com>
2020-02-24 09:30:20 +0100

    Delay block

--- rhesuton/effects.py
+++ rhesuton/effects.py
@@ -1,8 +1,16 @@
 """Audio effects."""
+import numpy as np
+
+from config import BUFFER_SIZE, SAMPLING_RATE
 from rhesuton.blocks import Block
+from rhesuton.constants import TAU
 from rhesuton.math import clip
 from rhesuton.oscillators import Oscillator
-from rhesuton.constants import TAU
+
+
+def blend(a, b, x):
+    """Dry / wet blend two signals together."""
+    return (1. - x) * a + x * b
 
 
 class Tremolo(Block):
@@ -36,3 +44,71 @@ class Tremolo(Block):
 
         # Set output
         self.output.value = env * samples
+
+
+class DelayBuffer:
+    def __init__(self, shape):
+        self.data = np.zeros(shape)
+        self.index = 0
+        self._cycleLength = shape[0]
+
+    @property
+    def cycleLength(self):
+        return self._cycleLength
+
+    @cycleLength.setter
+    def cycleLength(self, length):
+        assert 0 < length <= self.data.shape[0]
+        self._cycleLength = length
+        self.index %= length
+
+    def peek(self):
+        return self.data[self.index].copy()
+
+    def push(self, value):
+        self.data[self.index] = value
+        self.index = (self.index + 1) % self.cycleLength
+
+
+class Delay(Block):
+
+    """Simple 2 second delay.
+
+    Untested.
+
+    Todo:
+      - Very small delay times
+    """
+
+    MAX_DELAY = 2.
+
+    def __init__(self, delay=1., feedback=.1, drywet=.5):
+        assert delay <= self.MAX_DELAY
+        super().__init__(nInputs=4, nOutputs=1)
+        _, self.delay, self.feedback, self.drywet = self.inputs
+        self.delay.set_value(delay)
+        self.feedback.set_value(feedback)
+        self.drywet.set_value(drywet)
+
+        maxlen = self.delay_2_buffer_index(delay)
+        self.buffer = DelayBuffer((maxlen, BUFFER_SIZE))
+
+    @staticmethod
+    def delay_2_buffer_index(duration):
+        """Time duration -> buffer index."""
+        return int(duration * SAMPLING_RATE / BUFFER_SIZE)
+
+    def update(self):
+        # Fetch inputs
+        new = self.input.get_value()
+        delay = self.delay.get_value()
+        feedback = self.feedback.get_value()
+        drywet = self.drywet.get_value()
+
+        length = self.delay_2_buffer_index(delay)
+        self.buffer.cycleLength = length
+        old = self.buffer.peek()
+        self.buffer.push(new + feedback * old)
+
+        # Set output
+        self.output.set_value(blend(new, old, drywet))
