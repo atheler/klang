fa6bec4531e24cf02fea5272cabeb7f802160c79
atheler <atheler@users.noreply.github.com>
2020-03-03 22:45:48 +0100

    Ring buffer class with test cases

---
+++ klang/ring_buffer.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+
+class RingBuffer:
+
+    """Numpy ring buffer."""
+
+    def __init__(self, shape):
+        self.data = np.zeros(shape)
+        self.writeIdx = 0
+        self.readIdx = 0
+
+    @property
+    def length(self):
+        return self.data.shape[0]
+
+    def write(self, frames):
+        """Write some data to ring buffer."""
+        start = self.writeIdx
+        length = self.data.shape[0]
+        stop = (start + len(frames)) % self.length
+        self.writeIdx = stop
+        self[start:stop] = frames
+
+    def read(self, nFrames):
+        """Read `nFrames` from ring buffer."""
+        start = self.readIdx
+        stop = (start + nFrames) % self.data.shape[0]
+        self.readIdx = stop
+        return self[start:stop]
+
+    def unpack_slice(self, slc):
+        """Unpack slice `slice` in to tuple (start, stop, end)."""
+        if slc.start is None:
+            start = 0
+        else:
+            start = slc.start % self.length
+
+        if slc.stop is None:
+            stop = self.length
+        else:
+            stop = slc.stop % self.length
+
+        if slc.step is None:
+            step = 1
+        else:
+            step = slc.step
+
+        return start, stop, step
+
+    def __setitem__(self, key, value):
+        #print('__setitem__(%s, %s)' % (key, value))
+        if isinstance(key, slice):
+            start, stop, step = self.unpack_slice(key)
+            if start < stop:
+                self.data[start:stop:step] = value
+            else:
+                try:
+                    mid = (self.length - start) // step
+                    self.data[start::step] = value[:mid]
+                    self.data[:stop:step] = value[mid:]
+                except TypeError:
+                    self.data[start::step] = value
+                    self.data[:stop:step] = value
+
+        elif isinstance(key, int):
+            self.data[key % self.length] = value
+
+        else:
+            self.data[key] = value
+
+    def __getitem__(self, key):
+        #print('__getitem__(%s)' % key)
+        if isinstance(key, slice):
+            start, stop, step = self.unpack_slice(key)
+            if start < stop:
+                return self.data[start:stop:step]
+
+            return np.concatenate([
+                self.data[start::step],
+                self.data[:stop:step],
+            ])
+
+        if isinstance(key, int):
+            return self.data[key % self.length]
+
+        return self.data[key]
---
+++ tests/test_ring_buffer.py
@@ -0,0 +1,45 @@
+import unittest
+
+import numpy as np
+
+from klang.ring_buffer import RingBuffer
+
+
+class TestRingBuffer(unittest.TestCase):
+    def setUp(self):
+        self.ring = RingBuffer(10)
+        self.ring.data[:] = np.arange(10)
+
+    def test_getitem_with_index_and_wrap_around(self):
+        for i in range(-10, 20):
+            np.testing.assert_equal(self.ring[i], i % 10)
+
+    def test_getitem_with_slice(self):
+        np.testing.assert_equal(self.ring[:2], [0, 1])
+        np.testing.assert_equal(self.ring[8:], [8, 9])
+        np.testing.assert_equal(self.ring[2:8:2], [2, 4, 6])
+
+    def test_getitem_with_slice_and_wrap_around(self):
+        np.testing.assert_equal(self.ring[8:12], [8, 9, 0, 1])
+
+    def test_setitem_single_value_with_slice_and_wrap_aroud(self):
+        self.ring[8:14:2] = 666
+        np.testing.assert_equal(self.ring.data, [666, 1, 666, 3, 4, 5, 6, 7, 666, 9])
+
+    def test_setitem_multiple_values_with_slice_and_wrap_aroud(self):
+        self.ring[8:14:2] = 42, 43, 44
+        np.testing.assert_equal(self.ring.data, [43, 1, 44, 3, 4, 5, 6, 7, 42, 9])
+
+    def test_write_read(self):
+        ring = RingBuffer((10, 2))
+        ring.write(np.ones((1, 2)))
+        ring.write(np.ones((2, 2)) * 2)
+        ring.write(np.ones((3, 2)) * 3)
+
+        np.testing.assert_equal(ring.read(1), [[1, 1]])
+        np.testing.assert_equal(ring.read(1), [[2, 2]])
+        np.testing.assert_equal(ring.read(2), [[2, 2], [3, 3]])
+
+
+if __name__ == '__main__':
+    unittest.main()
