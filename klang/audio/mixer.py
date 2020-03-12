9e521e3df6a2da0648b8619004fcefaadec7007f
atheler <atheler@users.noreply.github.com>
2020-03-12 11:49:17 +0100

    Mixer with gain, StereoMixer WIP

--- klang/audio/mixer.py
+++ klang/audio/mixer.py
@@ -1,9 +1,17 @@
 """Mixer"""
 import numpy as np
 
-from klang.audio import MONO_SILENCE
+from klang.audio import MONO_SILENCE, STEREO_SILENCE
 from klang.blocks import Block
 from klang.constants import MONO, STEREO
+from klang.math import clip
+
+
+LEFT = -1.
+"""int: Maximum left panning value."""
+
+RIGHT = 1.
+"""int: Maximum right panning value."""
 
 
 class Mixer(Block):
@@ -14,20 +22,20 @@ class Mixer(Block):
       - Levels
       - Panning
       - Surround?
-      - Digital summing? Soft distortion with tanh?
-
-    Resources:
-      - https://dsp.stackexchange.com/questions/3581/algorithms-to-mix-audio-signals-without-clipping)
     """
 
     def __init__(self, nInputs=2, nOutputs=MONO):
         assert nOutputs in {MONO, STEREO}
         super().__init__(nInputs=nInputs, nOutputs=nOutputs)
+        self.gains = np.ones(nInputs)
+
+    def set_gain(self, channel, gain):
+        self.gains[channel] = clip(gain, 0., 1.)
 
     def update(self):
         signalSum = MONO_SILENCE.copy()
-        for channel in self.inputs:
-            signalSum += channel.get_value()
+        for gain, channel in zip(self.gains, self.inputs):
+            signalSum += gain * channel.get_value()
 
         if self.nInputs > 1:
             signalSum /= self.nInputs
@@ -36,5 +44,23 @@ class Mixer(Block):
             output.set_value(signalSum)
 
 
-class StereoMixer:
-    pass
+class StereoMixer(Mixer):
+    def __init__(self, nInputs=2):
+        raise NotImplementedError
+        super().__init__(nInputs, nOutputs=STEREO)
+        self.panning = np.zeros(nInputs)
+
+    def set_panning(self, channel, panning):
+        self.panning[channel] = clip(panning, LEFT, RIGHT)
+
+    def update(self):
+        # TODO(atheler): Make me!
+        signalSum = STEREO_SILENCE.copy()
+        for gain, channel in zip(self.gains, self.inputs):
+            signalSum += gain * channel.get_value()
+
+        if self.nInputs > 1:
+            signalSum /= self.nInputs
+
+        for output in self.outputs:
+            output.set_value(signalSum)
