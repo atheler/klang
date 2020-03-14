99192145346cb901b5927e87840d9c8891a258dd
atheler <atheler@users.noreply.github.com>
2020-03-14 14:46:00 +0100

    Minor

--- klang/audio/__init__.py
+++ klang/audio/__init__.py
@@ -1,4 +1,7 @@
-"""All things audio."""
+"""All things audio.
+
+In this module some helper constants / arrays for working with audio.
+"""
 import numpy as np
 
 from config import BUFFER_SIZE, SAMPLING_RATE
@@ -11,17 +14,18 @@ NYQUIST_FREQUENCY = SAMPLING_RATE // 2
 """int: Nyquist frequency."""
 
 MONO_SILENCE = np.zeros(BUFFER_SIZE)
-MONO_SILENCE.flags.writeable = False
 """array: Default array for mono silence."""
 
 STEREO_SILENCE = np.zeros((2, BUFFER_SIZE))
-STEREO_SILENCE.flags.writeable = False
 """array: Default array for stereo silence."""
 
 T = DT * np.arange(BUFFER_SIZE)
-T.flags.writeable = False
 """array: Buffer time points."""
 
 T1 = DT * np.arange(BUFFER_SIZE + 1)
-T1.flags.writeable = False
 """array: Buffer time points plus one (continuation)."""
+
+
+# Make all numpy ndarray's in this module read-only
+for _arr in [val for val in globals().values() if isinstance(val, np.ndarray)]:
+    _arr.setflags(write=False)
