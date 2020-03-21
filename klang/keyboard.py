0f46ffdc72bc783040a79dd88f829f16ff02f245
atheler <atheler@users.noreply.github.com>
2020-03-21 13:54:56 +0100

    Temperament switching inside MusicalKeyboard, refactor, doc

--- klang/keyboard.py
+++ klang/keyboard.py
@@ -1,5 +1,6 @@
 """Keyboard input."""
 import collections
+import string
 
 from pynput.keyboard import Listener
 
@@ -7,17 +8,24 @@ from klang.block import Block
 from klang.connections import MessageOutput
 from klang.constants import REF_OCTAVE, DODE
 from klang.math import clip
-from klang.messages import PitchNote
+from klang.messages import NewNote
+from klang.music.tunings import EQUAL_TEMPERAMENT, TEMPERAMENTS
 
 
 SILENT = 0.
-"""float: TODO."""
+"""float: Silent velocity value."""
 
 LOUDER = .1
 """float: Louder increment."""
 
 QUIETER = -.1
 """float: Quieter decrement."""
+
+TEMPERAMENTS = list(TEMPERAMENTS.values())
+"""list: Temperament values. Makes the first 10 temperament accessible."""
+
+DIGITS = set(string.digits)
+"""set: String digits."""
 
 
 class Keyboard(Block):
@@ -41,7 +49,7 @@ class Keyboard(Block):
             return
 
         self.keyState[key] = True
-        self.on_event(key)
+        self.on_key_event(key)
 
     def on_release(self, key):
         """On key release event handler with debouncing."""
@@ -50,15 +58,18 @@ class Keyboard(Block):
             return
 
         self.keyState[key] = False
-        self.on_event(key)
+        self.on_key_event(key)
 
-    def on_event(self, key):
+    def on_key_event(self, key):
+        """On key event. What to do on key event (pressed and released)."""
         self.output.send(key)
 
     def start(self):
+        """Start Keyboard worker thread."""
         self.keyWorker.start()
 
     def stop(self):
+        """Stop keyboard worker thread."""
         self.keyWorker.stop()
 
     def update(self):
@@ -76,8 +87,9 @@ class MusicalKeyboard(Keyboard):
     """Musical keyboard.
 
     Main output outputs Notes. Octaves can be changed with the 'y' and 'x' keys,
-    velocity can be changed via 'c', 'v'.  If others=True remaining key events
-    will be send to secondary output.
+    velocity can be changed via 'c', 'v'. Current tuning temperaments can be
+    switched via the number keys. If others=True remaining key events will be
+    send to a secondary output.
     """
 
     CHAR_2_BASE_PITCH = {
@@ -86,10 +98,12 @@ class MusicalKeyboard(Keyboard):
     }
     """dict: Keyboard character (str) -> Pitch number."""
 
-    def __init__(self, suppress=False, others=False):
+    def __init__(self, suppress=False, others=False,
+                 defaultTemperament=EQUAL_TEMPERAMENT):
         """Kwargs:
             others (bool): Send remaining key events to secondary message
                 output.
+            defaultTemperament (Temperament): Default tuning temperament.
         """
         super().__init__(suppress=suppress)
         self.others = others
@@ -99,44 +113,75 @@ class MusicalKeyboard(Keyboard):
 
         self.octave = REF_OCTAVE
         self.velocity = 1.
+        self.temperament = defaultTemperament
 
-    def on_event(self, key):
+    def play_note(self, char, noteOn=True):
+        """Play music note (note-on and note-off)."""
+        pitch = self.CHAR_2_BASE_PITCH[char] + self.octave * DODE
+        frequency = self.temperament.pitch_2_frequency(pitch)
+        velocity = self.velocity if noteOn else SILENT
+        note = NewNote(frequency, velocity, pitch)
+        self.output.send(note)
+
+    def change_octave(self, char):
+        """Change current octave up / down."""
+        if char == 'y':
+            fmt = 'One octave down to %d'
+            self.octave -= 1
+        else:
+            fmt = 'One octave up to %d'
+            self.octave += 1
+
+        print(fmt % self.octave)
+
+    def change_velocity(self, char):
+        """Increase / decrease current velocity."""
+        if char == 'c':
+            fmt = 'Decrease velocity to %.1f'
+            vel = self.velocity + QUIETER
+        else:
+            fmt = 'Increased velocity to %.1f'
+            vel = self.velocity + LOUDER
+
+        self.velocity = clip(vel, .1, 1.)
+        print(fmt % self.velocity)
+
+    def change_temperament(self, char):
+        """Change to another tuning temperament."""
+        try:
+            nr = int(char)
+        except ValueError:
+            return
+
+        if 0 <= nr < len(TEMPERAMENTS):
+            self.temperament = TEMPERAMENTS[nr]
+            print('Switched to %s' % self.temperament)
+
+    def bypass_other_keys(self, key):
+        """Relays remaining keys to outputs[1]."""
+        print('Other key', key)
+        self.outputs[1].send(key)
+
+    def on_key_event(self, key):
         try:
             char = key.char
         except AttributeError:
             char = None
 
         if char in self.CHAR_2_BASE_PITCH:
-            pitch = self.CHAR_2_BASE_PITCH[char] + self.octave * DODE
-            velocity = self.velocity if key.pressed else SILENT
-            note = PitchNote(pitch, velocity)
-            self.output.send(note)
-            return
+            return self.play_note(char, noteOn=key.pressed)
 
         if key.pressed:
             if char in {'y', 'x'}:
-                if char == 'y':
-                    fmt = 'One octave down to %d'
-                    self.octave -= 1
-                else:
-                    fmt = 'One octave up to %d'
-                    self.octave += 1
+                return self.change_octave(char)
 
-                print(fmt % self.octave)
-                return
+            if char in {'c', 'v'}:
+                return self.change_velocity(char)
 
-            elif char in {'c', 'v'}:
-                if char == 'c':
-                    fmt = 'Decrease velocity to %.1f'
-                    vel = self.velocity + QUIETER
-                else:
-                    fmt = 'Increased velocity to %.1f'
-                    vel = self.velocity + LOUDER
-
-                self.velocity = clip(vel, .1, 1.)
-                print(fmt % self.velocity)
-                return
+            if char in DIGITS:  # None in string.digits would fail!
+                return self.change_temperament(char)
 
         if self.others:
-            print('Other key', key)
-            self.outputs[1].send(key)
+            return self.bypass_other_keys(key)
+
+        return None
