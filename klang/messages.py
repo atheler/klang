98a25f59216f42b108704fdf2c11086143d29942
atheler <atheler@users.noreply.github.com>
2020-03-21 18:39:20 +0100

    Note from pitch

--- klang/messages.py
+++ klang/messages.py
@@ -1,6 +1,8 @@
 """Messages passed around in the system."""
 import collections
 
+from klang.music.tunings import EQUAL_TEMPERAMENT
+
 
 class Note(collections.namedtuple('Note', 'frequency velocity pitch')):
 
@@ -8,3 +10,13 @@ class Note(collections.namedtuple('Note', 'frequency velocity pitch')):
 
     def __new__(cls, frequency, velocity, pitch=None):
         return super().__new__(cls, frequency, velocity, pitch)
+
+    @classmethod
+    def from_pitch(cls, pitch, velocity=1., temperament=EQUAL_TEMPERAMENT):
+        """Create Note from pitch."""
+        freq = temperament.pitch_2_frequency(pitch)
+        return cls(freq, velocity, pitch)
+
+    def __str__(self):
+        return 'Note(frequency=%.1f, velocity=%.1f, pitch=%d)' % self
+        #return 'Note(pitch=%d, velocity=%.1f)' % (self.pitch, self.velocity)
