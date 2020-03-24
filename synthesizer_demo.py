cffc74b66cdd9a84f80f26a27dbfae4911023bbb
atheler <atheler@users.noreply.github.com>
2020-03-24 22:58:05 +0100

    Synthesizer demo

---
+++ synthesizer_demo.py
@@ -0,0 +1,27 @@
+"""Klang synthesizer demo."""
+from klang.audio.klanggeber import KlangGeber
+from klang.audio.synthesizer import MonophonicSynthesizer, PolyphonicSynthesizer
+from klang.keyboard import MusicalKeyboard
+from klang.audio.voices import OscillatorVoice
+from klang.audio.oscillators import Oscillator
+from klang.audio.envelope import ADSR
+
+
+# Params
+MONOPHONIC = True
+
+
+keyboard = MusicalKeyboard()
+keyboard.start()  # Start keyboard daemon thread
+
+#synthesizer = Synthesizer()
+env = ADSR(.1, .2, .3, .4)
+osc = Oscillator()
+voice = OscillatorVoice(env, osc)
+
+cls = MonophonicSynthesizer if MONOPHONIC else PolyphonicSynthesizer
+synthesizer = cls(voice)#, policy='new')
+
+with KlangGeber(nOutputs=1) as dac:
+    keyboard.output.connect(synthesizer.input)
+    synthesizer.output.connect(dac.input)
