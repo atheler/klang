62a744d58b2aa2125ed1ed0687da73b84150e53a
atheler <atheler@users.noreply.github.com>
2020-03-27 18:23:32 +0100

    New oscillator with phasor

--- klang/audio/oscillators.py
+++ klang/audio/oscillators.py
@@ -1,45 +1,97 @@
 """Oscillator audio blocks."""
-from klang.audio import MONO_SILENCE
+from config import BUFFER_SIZE
+from klang.audio import DT, get_silence, get_time
+from klang.audio.waves import sine
 from klang.block import Block
-from klang.audio.waves import sample_wave
+from klang.constants import TAU
+
+
+class Phasor(Block):
+
+    """Oscillating phase driver."""
+
+    def __init__(self, frequency=1., startPhase=0.):
+        """Kwargs:
+            frequency (float): Phasor frequency.
+            startPhase (float): Starting phase.
+        """
+        super().__init__(nOutputs=1)
+        self.frequency = frequency
+        self.output.set_value(startPhase)
+
+    @property
+    def currentPhase(self):
+        """Current phase of the phasor."""
+        return self.output.value
+
+    def update(self):
+        delta = TAU * self.frequency * DT * BUFFER_SIZE
+        self.output._value += delta
+        self.output._value %= TAU
+
+    def __deepcopy__(self, memo):
+        return type(self)(
+            frequency=self.frequency,
+            startPhase=self.currentPhase,
+        )
 
 
 class Oscillator(Block):
 
-    """Basic oscillator."""
+    """Wave function oscillator."""
 
-    def __init__(self, frequency=440.):
-        super().__init__(nInputs=1, nOutputs=1)
-        self.frequency = self.input
-        self.frequency.set_value(frequency)
-        self.output.set_value(MONO_SILENCE)
-        self.currentPhase = 0.
+    def __init__(self, frequency=440., wave_func=sine, startPhase=0.,
+                 shape=BUFFER_SIZE):
+        """Kwargs:
+            frequency (float): Oscillator frequency.
+            wave_func (function): Wave function: wave_func(phases) -> values.
+            startPhase (float): Starting phase.
+            shape (int): Output value shape.
+        """
+        assert shape in {1, BUFFER_SIZE}
+        super().__init__(nOutputs=1)
+        self.wave_func = wave_func
+        self.shape = shape
+
+        silence = get_silence(shape)
+        self.output.set_value(silence)
+        self.phasor = Phasor(frequency, startPhase)
+
+    @property
+    def frequency(self):
+        """Get current frequency."""
+        return self.phasor.frequency
+
+    @frequency.setter
+    def frequency(self, frequency):
+        """Set frequency."""
+        self.phasor.frequency = frequency
 
     def sample(self):
-        freq = self.frequency.get_value()
-        samples, self.currentPhase = sample_wave(
-            freq,
-            self.currentPhase,
-        )
-        return samples
+        """Calculate current samples."""
+        t = get_time(self.shape)
+        phase = TAU * self.frequency * t + self.phasor.currentPhase
+        return self.wave_func(phase)
 
     def update(self):
-        samples = self.sample()
-        self.output.set_value(samples)
+        values = self.sample()
+        self.output.set_value(values)
+        self.phasor.update()
 
     def __deepcopy__(self, memo):
-        oscCopy = type(self)(frequency=self.frequency.value)
-        oscCopy.currentPhase = self.currentPhase
-        oscCopy.output.set_value(self.output.value)
-        return oscCopy
+        return type(self)(
+            frequency=self.frequency,
+            wave_func=self.wave_func,
+            startPhase=self.phasor.currentPhase,
+            shape=self.shape,
+        )
 
 
 class Lfo(Oscillator):
 
-    """Simple LFO. Same as Oscillator but output value range is [0., 1.]."""
-
-    def __init__(self, frequency=1.):
-        super().__init__(frequency)
+    """Simple low frequency oscillator (LFO). Same as Oscillator but output
+    value range is [0., 1.].
+    """
 
     def update(self):
         samples = self.sample()
---
+++ tests/test_oscillators.py
@@ -0,0 +1,61 @@
+import unittest
+
+import numpy as np
+
+from config import BUFFER_SIZE, SAMPLING_RATE
+from klang.audio import DT
+from klang.audio.oscillators import Phasor, Oscillator, Lfo
+from klang.constants import TAU
+
+
+DELTA = DT * BUFFER_SIZE
+
+
+class TestPhaseor(unittest.TestCase):
+    def test_phasor(self):
+        freq = .5
+        phasor = Phasor(frequency=freq)
+
+        self.assertEqual(phasor.currentPhase, 0.)
+
+        phasor.update()
+
+        self.assertEqual(phasor.currentPhase, TAU*freq*DELTA)
+
+    def test_phase_wrap(self):
+        freq = 10.
+        phasor = Phasor(frequency=freq)
+        atLeastOneCycle = int((SAMPLING_RATE / freq) // BUFFER_SIZE) + 1
+        for _ in range(atLeastOneCycle):
+            phasor.update()
+
+        self.assertAlmostEqual(phasor.currentPhase, atLeastOneCycle*TAU*freq*DELTA % TAU)
+
+
+class TestOscillator(unittest.TestCase):
+    def test_400_hz(self):
+        freq = 440.
+        osc = Oscillator(frequency=freq)
+
+        t = DT * np.arange(BUFFER_SIZE)
+        ref = np.sin(TAU * freq * t)
+        osc.update()
+
+        np.testing.assert_equal(osc.output.value, ref)
+
+
+class TestLfo(unittest.TestCase):
+    def test_400_hz(self):
+        freq = 440.
+        lfo = Lfo(frequency=freq)
+
+        t = DT * np.arange(BUFFER_SIZE)
+        ref = np.sin(TAU * freq * t)
+        ref = (ref + 1.) / 2.
+        lfo.update()
+
+        np.testing.assert_equal(lfo.output.value, ref)
+
+
+if __name__ == '__main__':
+    unittest.main()
