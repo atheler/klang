514200960a3049244fbad406f108eedb48efef44
atheler <atheler@users.noreply.github.com>
2020-09-09 14:38:52 +0200

    Doc and third party logger surpress

--- klang/__init__.py
+++ klang/__init__.py
@@ -8,3 +8,9 @@ import logging
 __author__ = 'atheler'
 __version__ = '0.1.3'
 logging.basicConfig(level=logging.DEBUG)
+
+
+# Surpress parse loggers
+for name in logging.root.manager.loggerDict:
+    if 'parso' in name:
+        logging.getLogger(name).disabled = True
